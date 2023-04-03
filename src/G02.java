a5c34dd5a521bb1fde2ca42707e662f474157068
BurhanSemiz <77208168+burhansemiz2020@users.noreply.github.com>
2023-04-03 23:57:06 +0200

    GITHUB04

---
+++ src/G02.java
@@ -0,0 +1,13 @@
+public class G02 {
+    public static void main(String[] args) {
+        /*
+        echo "# GITHUB04" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/burhansemiz2020/GITHUB04.git
+git push -u origin main
+         */
+    }
+}
